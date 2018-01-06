a9b04188b09a26933565db5da7452cbad97ed3e1
ka.yama0fa <ka.yama0fa@gmail.com>
2018-01-07 03:58:05 +0900

    Update tramp config.

--- inits/94_tramp.el
+++ inits/94_tramp.el
@@ -1,6 +1,9 @@
 ;; C+x f /ssh:user@host:/path/to/file
-(setq tramp-default-method "ssh")
-;; TRAMPリモート接続時のPATH設定を改善させる
-;; (例:リモートでのhg(version control)作業)
-;; リモートのバッファ上でM-Shift-! echo PATHをしてみるとよく分かる。
-(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+(use-package tramp
+  :config
+  (setq tramp-default-method "ssh")
+  ;; TRAMPリモート接続時のPATH設定を改善させる
+  ;; (例:リモートでのhg(version control)作業)
+  ;; リモートのバッファ上でM-Shift-! echo PATHをしてみるとよく分かる。
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+)
