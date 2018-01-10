f290b52f3d647569a4888166331626d2990a21ab
ka.yama0fa <ka.yama0fa@gmail.com>
2018-01-10 13:06:19 +0900

    Updated mode alist.

--- inits/90_mode_alist.el
+++ inits/90_mode_alist.el
@@ -1,17 +1,1 @@
-;; ;; 以下、一応残しているが不要なはず・・・
-;; (setq auto-mode-alist
-;;       (append (list
-;;                 '("\\.sql$"   . sql-mode)
-;;                 '("Makefile.am$" . makefile-mode)
-;;                 '("Makefile.in$" . makefile-mode)
-;;                 ;; '("\\.js$"   . javascript-mode)
-;;                 '("\\.js$"   . js2-mode)
-;;                 '("\\.json$" . javascript-mode)
-;;                 '("\\.xml$"   . nxml-mode)
-;;                 '("\\.conf$"   . conf-space-mode)
-;;                 '("\\.py$"    . python-mode)
-;;                 '("\\.py.in$" . python-mode)
-;;                 '("\\.yml$" . yaml-mode)
-;;                 auto-mode-alist)
-;;               )
-;;       )
+(add-to-list 'auto-mode-alist '("Cask" . emacs-lisp-mode))
