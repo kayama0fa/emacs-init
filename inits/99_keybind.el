4e0f3d62a0b4749c100e24253e806106f73187ae
ka.yama0fa <ka.yama0fa@gmail.com>
2018-01-10 16:58:30 +0900

    Add memo. Organize comments.

--- inits/99_keybind.el
+++ inits/99_keybind.el
@@ -1,10 +1,17 @@
-;;; キーバインド
-(define-key global-map "\C-h" 'delete-backward-char) ; 削除
-;;(define-key global-map "\C-z" 'undo)                 ; undo(C-/)
-(define-key global-map "\C-c;" 'comment-region)      ; コメントアウト
-(define-key global-map "\C-c:" 'uncomment-region)    ; コメント解除
-(define-key global-map "\C-\\" nil) ; \C-\の日本語入力の設定を無効にする
+;; keybind
+(define-key global-map "\C-h" 'delete-backward-char)  ;; 削除
+(define-key global-map "\C-c;" 'comment-region)       ;; コメントアウト
+(define-key global-map "\C-c:" 'uncomment-region)     ;; コメント解除
+(define-key global-map "\C-\\" nil)                   ;; \C-\の日本語入力の設定を無効にする
 (define-key global-map "\C-cc" `compile)
-(global-set-key "\C-x\C-a" 'add-change-log-entry)     ; Change-Logを作成する
-;; (define-key global-map "\C-x\C-c" nil) ; ショートカットによる修正を無効化
-(global-set-key "\C-cg" 'imenu)	;; C-c gで開いているコードの関数一覧を表示する
+(global-set-key "\C-x\C-a" 'add-change-log-entry)     ;; Change-Logを作成する
+(global-set-key "\C-cg" 'imenu)                       ;; C-c gで開いているコードの関数一覧を表示する
+
+;; (define-key global-map "\C-z" 'undo)                  ;; undo(C-/)
+;; (define-key global-map "\C-x\C-c" nil)                ;; ショートカットによる修正を無効化
+
+;; memo:
+;; * あるキーに何が割り当てられているか調べる
+;;     * M-x describe-key
+;; * 割り当てられているキーの一覧
+;;     * M-x describe-bindings
