a1c2145d3b4bddcae50e883662648bcfe3f58509
ka.yama0fa <ka.yama0fa@gmail.com>
2018-01-10 12:15:46 +0900

    Updated config.

--- inits/88_tags.el
+++ inits/88_tags.el
@@ -8,7 +8,7 @@
                (local-set-key (kbd "M-t") 'helm-gtags-find-tag)
                (local-set-key (kbd "M-r") 'helm-gtags-find-rtag)
                (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
-               (local-set-key (kbd "C-t") 'helm-gtags-pop-stack)
+               (local-set-key (kbd "M-T") 'helm-gtags-pop-stack)
                ))
 )
 
