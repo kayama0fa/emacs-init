8543c1b5edaf57db9d1ebaf43e97cd76e44b3b68
kayama0fa <ka.yama0fa@gmail.com>
2019-01-21 16:07:14 +0900

    Update company-mode conf.

--- inits/92_company.el
+++ inits/92_company.el
@@ -1,5 +1,11 @@
-(global-company-mode)
+(global-company-mode) ;; 全ての場合でcompany-mode適用
 ;; or (add-hook 'python-mode-hook 'company-mode) など
-(setq company-idle-delay 0.2) ; デフォルトは0.5
-(setq company-minimum-prefix-length 2) ; デフォルトは4
+
+;; 任意のモードでcomanyを無効にする。モード名は "M-: major-mode" で得られる。
+(setq company-global-modes '(not org-mode))
+(setq company-global-modes '(not magit-status-mode))
+(setq company-global-modes '(not shell-mode))
+
+;;(setq company-idle-delay 0.2) ; デフォルトは0.5
+;;(setq company-minimum-prefix-length 3) ; デフォルトは4
 (setq company-selection-wrap-around t) ; 候補の一番下でさらに下に行こうとすると一番上に戻る
