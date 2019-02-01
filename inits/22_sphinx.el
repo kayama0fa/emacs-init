5c26b48148fd1603a31c0381692e1acb9ee25882
kayama0fa <ka.yama0fa@gmail.com>
2019-02-01 12:21:04 +0900

    Improve rst-mode config.

--- inits/22_sphinx.el
+++ inits/22_sphinx.el
@@ -1,5 +1,25 @@
-;; 全部スペースでインデントしましょう
+;; 参考:
+;;   * http://docutils.sourceforge.net/docs/user/emacs.html
+
+;; 全部半角スペースでインデント
 (add-hook 'rst-mode-hook '(lambda() (setq indent-tabs-mode nil)))
 
 (add-to-list 'auto-mode-alist '("\\.rst\\'" . rst-mode))
 (add-to-list 'auto-mode-alist '("\\.rest\\'" . rst-mode))
+
+;; Section titleの長さと"=,-,^"等を合わせる。
+;; rst-adjust (C-c C-a C-a)
+;;
+;; 参考: http://www.sphinx-doc.org/en/master/usage/restructuredtext/basics.html#sections
+;; * # with overline, for parts
+;; * * with overline, for chapters
+;; * =, for sections
+;; * -, for subsections
+;; * ^, for subsubsections
+;; * ", for paragraphs
+(custom-set-variables '(rst-preferred-adornments
+                        `( (?= simple 0)
+                           (?- simple 0)
+                           (?^ simple 0)
+                           (?\" simple 0)
+                          )))
